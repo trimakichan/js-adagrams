cbef36587a9353cbc099eed3eb916f580d2d333c
Makiko <trimakichan@gmail.com>
2025-11-20 14:51:55 -0800

    WIP: wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,26 @@
+import { LETTER_POOL, SCORE_CHART } from './constants.js';
+
+const HAND_SIZE = 10;
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const allLetters = [];
+  const hand = [];
+
+  for (const [letter, count] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i < count; i++) {
+      allLetters.push(letter);
+    }
+  }
+
+  for (let i = 0; i < HAND_SIZE; i++) {
+    const randomIndex = Math.floor(Math.random() * allLetters.length);
+    hand.push(allLetters[randomIndex]);
+    allLetters[randomIndex] = allLetters[allLetters.length - 1];
+    allLetters.pop();
+  };
+
+  return hand;
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -13,3 +34,7 @@ export const scoreWord = (word) => {
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 4
 };
+
+drawLetters();
+
+
